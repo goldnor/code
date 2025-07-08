62ea1c052209e1e5244078078512771fd6ff712d
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-08 15:22:36 +0200

    Rendered image setup

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,18 @@ use code::color::{Color, write_color};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Image
 
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    const IMAGE_HEIGHT: i32 = {
+        let image_height = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+        if image_height < 1 { 1 } else { image_height }
+    };
+
+    // Viewport widths less than one are ok since they are real valued.
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (IMAGE_WIDTH as f64) / (IMAGE_HEIGHT as f64);
 
     // Render
 
