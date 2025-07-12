86c110ccad16979ec62dce945725abab72499799
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-12 10:56:38 +0200

    The hittable_list class

---
+++ src/hittable_list.rs
@@ -0,0 +1,34 @@
+use std::rc::Rc;
+
+use crate::{
+    hittable::{HitRecord, Hittable},
+    ray::Ray,
+};
+
+#[derive(Default)]
+pub struct HittableList {
+    pub objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    pub fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object);
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord> {
+        self.objects
+            .iter()
+            .filter_map(|obj| obj.hit(r, ray_tmin, ray_tmax))
+            .min_by(|a, b| a.t.partial_cmp(&b.t).expect("no NaN value"))
+    }
+}
