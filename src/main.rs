47441cbbe4cb464b6f29e82801d47aba101092f2
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-12 08:53:44 +0200

    Rendering surface normals on a sphere

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ use code::{
     vec3::{Point3, Vec3, dot, unit_vector},
 };
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> Option<f64> {
     let oc = center - r.origin();
     let a = dot(r.direction(), r.direction());
     let b = -2.0 * dot(r.direction(), oc);
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant >= 0.0
+    (discriminant >= 0.0).then(|| (-b - f64::sqrt(discriminant)) / (2.0 * a))
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    if let Some(t) = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction = unit_vector(r.direction());
