9e1b0c216e67d1bcf41de6ee6c449758c9cc33e5
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-12 12:05:23 +0200

    The rtweekend.h common header

---
+++ src/prelude.rs
@@ -0,0 +1,14 @@
+pub use log::*;
+
+// Rust Std usings
+
+pub use std::rc::Rc;
+
+// Constants
+
+pub const INFINITY: f64 = f64::INFINITY;
+pub const PI: f64 = std::f64::consts::PI;
+
+// Common Headers
+
+pub use crate::{color::*, ray::*, vec3::*};
