ad94c6aff825a9cf5c9a0983bfefc7a5e3900725
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-14 23:11:14 +0200

    he random_unit_vector() function, version two

--- src/vec3.rs
+++ src/vec3.rs
@@ -192,7 +192,7 @@ pub fn random_unit_vector() -> Vec3 {
     loop {
         let p = Vec3::random_range(-1.0, 1.0);
         let lensq = p.length_squared();
-        if lensq <= 1.0 {
+        if 1e-160 < lensq && lensq <= 1.0 {
             return p / f64::sqrt(lensq);
         }
     }
