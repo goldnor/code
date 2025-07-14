f9b02376bc5c4d4f7337d6fef6903506086925da
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-14 09:20:06 +0200

    The multi-sample write_color() function

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,4 @@
-use crate::vec3::Vec3;
+use crate::prelude::*;
 
 pub type Color = Vec3;
 
@@ -7,9 +7,12 @@ pub fn write_color(mut out: impl std::io::Write, pixel_color: Color) -> std::io:
     let g = pixel_color.y();
     let b = pixel_color.z();
 
-    let rbyte = (255.999 * r) as i32;
-    let gbyte = (255.999 * g) as i32;
-    let bbyte = (255.999 * b) as i32;
+    // Translate the [0,1] component values to the byte range [0,255].
+    const INTENSITY: Interval = Interval::new(0.000, 0.999);
+    let rbyte = (256.0 * INTENSITY.clamp(r)) as i32;
+    let gbyte = (256.0 * INTENSITY.clamp(g)) as i32;
+    let bbyte = (256.0 * INTENSITY.clamp(b)) as i32;
 
+    // Write out the pixel color components.
     writeln!(out, "{rbyte} {gbyte} {bbyte}")
 }
