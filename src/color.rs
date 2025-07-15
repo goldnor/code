39f58a27fa6c25081163b184082bc7805a2dabcd
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-15 09:43:45 +0200

    write_color(), with gamma correction

--- src/color.rs
+++ src/color.rs
@@ -2,10 +2,24 @@ use crate::prelude::*;
 
 pub type Color = Vec3;
 
+#[inline]
+pub fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        f64::sqrt(linear_component)
+    } else {
+        0.0
+    }
+}
+
 pub fn write_color(mut out: impl std::io::Write, pixel_color: Color) -> std::io::Result<()> {
     let r = pixel_color.x();
     let g = pixel_color.y();
     let b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    let r = linear_to_gamma(r);
+    let g = linear_to_gamma(g);
+    let b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     const INTENSITY: Interval = Interval::new(0.000, 0.999);
