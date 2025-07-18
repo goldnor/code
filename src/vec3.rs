e8bbb4e688c7e3a8b3fc4f20c650cf43c5b9f71c
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-18 10:32:21 +0200

    Refraction function

--- src/vec3.rs
+++ src/vec3.rs
@@ -220,6 +220,15 @@ pub fn reflect(v: Vec3, n: Vec3) -> Vec3 {
 }
 
 #[inline]
+pub fn refract(uv: Vec3, n: Vec3, etai_over_etat: f64) -> Vec3 {
+    let cos_theta = f64::min(dot(-uv, n), 1.0);
+    let r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    let r_out_parallel = -f64::sqrt(f64::abs(1.0 - r_out_perp.length_squared())) * n;
+
+    r_out_perp + r_out_parallel
+}
+
+#[inline]
 pub fn random_in_unit_disk() -> Vec3 {
     loop {
         let p = Vec3::new(
