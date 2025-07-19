c702619b19462b2bff877076a01333fd974613fe
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-19 11:21:05 +0200

    Scene with alternate viewpoint

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,35 @@ use code::{
 fn main() -> std::io::Result<()> {
     let mut world = HittableList::new();
 
-    let r = f64::cos(PI / 4.0);
-
-    let material_left = Rc::new(Lambertian::new(Color::new(0.0, 0.0, 1.0)));
-    let material_right = Rc::new(Lambertian::new(Color::new(1.0, 0.0, 0.0)));
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dielectric::new(1.5));
+    let material_bubble = Rc::new(Dielectric::new(1.0 / 1.5));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Rc::new(Sphere::new(
-        Point3::new(-r, 0.0, -1.0),
-        r,
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.2),
+        0.5,
+        material_center,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
         material_left,
     )));
     world.add(Rc::new(Sphere::new(
-        Point3::new(r, 0.0, -1.0),
-        r,
+        Point3::new(-1.0, 0.0, -1.0),
+        0.4,
+        material_bubble,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
         material_right,
     )));
 
@@ -33,5 +49,8 @@ fn main() -> std::io::Result<()> {
         .with_samples_per_pixel(100)
         .with_max_depth(50)
         .with_vfov(90.0)
+        .with_lookfrom(Point3::new(-2.0, 2.0, 1.0))
+        .with_lookat(Point3::new(0.0, 0.0, -1.0))
+        .with_vup(Point3::new(0.0, 1.0, 0.0))
         .render(&world)
 }
